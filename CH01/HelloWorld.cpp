cf20e6082438c9ca495a5006d936c18e69dafeb7
c12402 <cjm8685@gmail.com>
2025-07-11 13:14:04 +0900

    Initial commit

---
+++ CH01/HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // iostream이라는 헤더를 포함
+
+using namespace std; // 네임스페이스설명 std::cout
+
+int main() { // entry point
+	cout << "Hello World" << endl;
+	//printf("Hello World by printf");
+
+	return 0; // OS에게 프로그램 종료를 알려주는 역할
+}
