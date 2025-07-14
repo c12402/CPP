c6f594c2492953b0de55ccbf664c5c909c9881f8
c12402 <cjm8685@gmail.com>
2025-07-14 10:46:51 +0900

    NumberGuess

---
+++ CH07/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+int main() {
+    std :: random_device rd;
+    std :: mt19937 gen(rd());
+    std :: uniform_int_distribution<> distrib(1, 99); // [1, 99]
+
+    int number = distrib(gen);
+
+    while (1) {
+        int guess;
+        cout << "입력: ";
+        cin >> guess;
+
+        if (guess == number) {
+            cout << "정답!" << endl;
+            break;
+        }    
+        else if (guess > number) {
+            cout << "너무 커요!" << endl;
+        }
+        else { // if(guess < number)
+            cout << "너무 작아요!" << endl;
+        }    
+    }
+
+    return 0;
+}
