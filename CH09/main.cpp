215704ad9589c803f1bed9f0c3563b21f31dcd30
c12402 <cjm8685@gmail.com>
2025-07-15 16:35:31 +0900

    함수 - modified

--- CH09/main.cpp
+++ CH09/main.cpp
@@ -2,21 +2,24 @@
 
 using namespace std;
 
-int Add(int a, int b) {
+int Add(int a, int b) { // 선언과 정의 분리 가능
     return a + b;
 }
 
-void Add(int a, int b, int *c) {
+void Add(int a, int b, int *c, int *d) { // 반환 자료형이 지정되지 않았음
     *c = a + b;
+    *d = a - b;
 }
 
 int main() {
     cout << Add(1, 2) << endl;
 
     int sum;
-    Add(4, 5, &sum);
+    int sub;
+    Add(4, 5, &sum, &sub);
 
     cout << sum << endl;
 
     return 0;
 }
+
